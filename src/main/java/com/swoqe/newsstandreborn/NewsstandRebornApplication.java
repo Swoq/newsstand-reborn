6b2458391539da2f9a9bf172565e50411681a732
Mike <misha.kratyuk@gmail.com>
2021-08-05 10:56:42 +0300

    Application initialization

---
+++ src/main/java/com/swoqe/newsstandreborn/NewsstandRebornApplication.java
@@ -0,0 +1,13 @@
+package com.swoqe.newsstandreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsstandRebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsstandRebornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swoqe/newsstandreborn/NewsstandRebornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swoqe.newsstandreborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsstandRebornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
